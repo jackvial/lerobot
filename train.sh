63eb2b135ca572fdf6a8d39640ab4683c4952949
Jack Vial <vialjack@gmail.com>
2024-08-18 17:16:42 -0400

    configure training

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python lerobot/scripts/train.py   dataset_repo_id=jackvial/koch_pick_and_place_pistachio_2_e10   policy=act_koch_real   env=koch_real   hydra.run.dir=outputs/train/act_koch_pick_and_place_pistachio_2_e10_2   hydra.job.name=act_koch_pick_and_place_pistachio_2_e10_2   device=cuda   wandb.enable=false
+python lerobot/scripts/train.py   dataset_repo_id=jackvial/koch_pick_and_place_pistachio_8_e100   policy=act_koch_real   env=koch_real   hydra.run.dir=outputs/train/act_koch_pick_and_place_pistachio_8_e100   hydra.job.name=act_koch_pick_and_place_pistachio_8_e100   device=cuda   wandb.enable=false
