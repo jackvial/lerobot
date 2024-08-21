c814db08cc29d090cb3ef83ee017d906b2a2b642
Jack Vial <vialjack@gmail.com>
2024-08-21 08:38:20 -0400

    correct dataset name

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-JOB_NAME="act_koch_pick_and_place_pistachio_8_e100_2_and_10_e20_and_11_e20_002"
+JOB_NAME="act_koch_pick_and_place_pistachio_8_e100_2_and_10_e20_and_11_e20_004"
 
 # Run the command with nohup
 nohup python lerobot/scripts/train.py \
-	'dataset_repo_id=[jackvial/koch_pick_and_place_pistachio_8_e10, jackvial/koch_pick_and_place_pistachio_10_e20, jackvial/koch_pick_and_place_pistachio_11_e20]' \
+	'dataset_repo_id=[jackvial/koch_pick_and_place_pistachio_8_e100, jackvial/koch_pick_and_place_pistachio_10_e20, jackvial/koch_pick_and_place_pistachio_11_e20]' \
 	policy=act_koch_real \
 	env=koch_real \
 	hydra.run.dir=outputs/train/$JOB_NAME \
