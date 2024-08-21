80e9af2bef4bc1b2bf45201363477794441e94e0
Jack Vial <vialjack@gmail.com>
2024-08-20 21:36:33 -0400

    name

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the job name variable at the top of the script
-JOB_NAME="act_koch_pick_and_place_pistachio_8_e100_2_and_10_e20_002"
+JOB_NAME="act_koch_pick_and_place_pistachio_8_e100_2_and_10_e20_and_11_e20_001"
 
 python lerobot/scripts/train.py \
 'dataset_repo_id=[jackvial/koch_pick_and_place_pistachio_8_e100, jackvial/koch_pick_and_place_pistachio_10_e20, jackvial/koch_pick_and_place_pistachio_11_e20]' \
