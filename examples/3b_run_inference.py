3eb24313f0153523a9d0a77724feb1cd4ab82a35
Jack Vial <vialjack@gmail.com>
2024-09-04 20:07:07 -0400

    config

--- examples/3b_run_inference.py
+++ examples/3b_run_inference.py
@@ -6,6 +6,7 @@ import torch
 from lerobot.common.robot_devices.robots.factory import make_robot
 from lerobot.common.policies.act.modeling_act import ACTPolicy
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
+from lerobot.common.policies.vqbet.modeling_vqbet import VQBeTPolicy
 from lerobot.common.utils.utils import init_hydra_config
 
 
@@ -23,49 +24,62 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser(description='Run inference with ACTPolicy')
-    parser.add_argument('--inference_time', type=int, default=60, help='Duration of inference in seconds')
+    parser.add_argument('--inference_time', type=int, default=15, help='Duration of inference in seconds')
     parser.add_argument('--fps', type=int, default=15, help='Frames per second')
+    parser.add_argument('--policy_type', type=str, help='Policy (act|diffision|vqbet)')
     parser.add_argument('--device', type=str, default='cuda', help='Device to run inference on')
     parser.add_argument('--modelname-or-path', type=str, default='outputs/train/act_koch_pick_and_place_pistachio_8_e100_2_and_10_e20_and_11_e20_004/checkpoints/last/pretrained_model', help='Path to the model')
     args = parser.parse_args()
 
     robot_path = "lerobot/configs/robot/koch_jack.yaml"
 
-    policy = DiffusionPolicy.from_pretrained(args.modelname_or_path)
-    policy.to(args.device)
+    if not args.policy_type:
+        raise ValueError("Policy must be provided")
 
-    robot_cfg = init_hydra_config(robot_path)
-    robot = make_robot(robot_cfg)
-    robot.connect()
+    if args.policy_type == "act":
+        policy = ACTPolicy.from_pretrained(args.modelname_or_path, cache_dir="model_cache")
+    elif args.policy_type == "diffusion":
+        policy = DiffusionPolicy.from_pretrained(args.modelname_or_path, cache_dir="model_cache")
+    elif args.policy_type == "vqbet":
+        policy = VQBeTPolicy.from_pretrained(args.modelname_or_path, cache_dir="model_cache")
 
-    for _ in range(args.inference_time * args.fps):
+    print(policy)
 
-        start_time = time.perf_counter()
 
-        # Read the follower state and access the frames from the cameras
-        observation = robot.capture_observation()
+    # policy.to(args.device)
 
-        # Convert to pytorch format: channel first and float32 in [0,1]
-        # with batch dimension
-        for name in observation:
-            if "image" in name:
-                observation[name] = observation[name].type(torch.float32) / 255
-                observation[name] = observation[name].permute(2, 0, 1).contiguous()
-            observation[name] = observation[name].unsqueeze(0)
-            observation[name] = observation[name].to(args.device)
+    # robot_cfg = init_hydra_config(robot_path)
+    # robot = make_robot(robot_cfg)
+    # robot.connect()
 
-        # Compute the next action with the policy
-        # based on the current observation
-        action = policy.select_action(observation)
+    # for _ in range(args.inference_time * args.fps):
 
-        # Remove batch dimension
-        action = action.squeeze(0)
+    #     start_time = time.perf_counter()
 
-        # Move to cpu, if not already the case
-        action = action.to("cpu")
+    #     # Read the follower state and access the frames from the cameras
+    #     observation = robot.capture_observation()
 
-        # Order the robot to move
-        robot.send_action(action)
+    #     # Convert to pytorch format: channel first and float32 in [0,1]
+    #     # with batch dimension
+    #     for name in observation:
+    #         if "image" in name:
+    #             observation[name] = observation[name].type(torch.float32) / 255
+    #             observation[name] = observation[name].permute(2, 0, 1).contiguous()
+    #         observation[name] = observation[name].unsqueeze(0)
+    #         observation[name] = observation[name].to(args.device)
 
-        dt_s = time.perf_counter() - start_time
-        busy_wait(1 / args.fps - dt_s)
+    #     # Compute the next action with the policy
+    #     # based on the current observation
+    #     action = policy.select_action(observation)
+
+    #     # Remove batch dimension
+    #     action = action.squeeze(0)
+
+    #     # Move to cpu, if not already the case
+    #     action = action.to("cpu")
+
+    #     # Order the robot to move
+    #     robot.send_action(action)
+
+    #     dt_s = time.perf_counter() - start_time
+    #     busy_wait(1 / args.fps - dt_s)
